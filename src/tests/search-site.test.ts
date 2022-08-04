7b0bf109261dcb83bbad3a9a27dea80edc162f3d
Guilherme Iazzetta <guilherme.iazzetta@gmail.com>
2022-08-04 20:48:08 -0300

    unificando o teste de status code e response

--- src/tests/search-site.test.ts
+++ src/tests/search-site.test.ts
@@ -1,15 +1,9 @@
 import { fakeSites } from "../infra/data-sources/sites";
 import { makeLoadSearchSiteController } from "../main/factories/search-site";
 
-test('search sites status code', async () => {
-    const search_text = '';
+test('search sites status code and response', async () => {
     const controller = makeLoadSearchSiteController()
-    const httpResponse = await controller.handle(search_text)
+    const httpResponse = await controller.handle('')
     expect(httpResponse.statusCode).toBe(200);
-});
-test('search sites data', async () => {
-    const search_text = 'test';
-    const controller = makeLoadSearchSiteController()
-    const httpResponse = await controller.handle(search_text)
     expect(httpResponse.data).toStrictEqual(fakeSites);
 });
