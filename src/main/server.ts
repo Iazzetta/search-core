6b977dee530524566f459640ee50e5f56771006e
Guilherme Iazzetta <guilherme.iazzetta@gmail.com>
2022-08-04 23:05:14 -0300

    add doc api, change package.json description

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,14 +1,23 @@
-import express from 'express'
+/* Internal Imports */
 import { env } from './config/env'
 import { makeLoadAddSiteController } from './factories/add-site';
 import { makeLoadSearchSiteController } from './factories/search-site';
 
+/* Third Party Imports */
+import express from 'express'
+
+/* Config Express */
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-/* Add new site */
+/**
+ * Add site
+ * @constructor
+ * @param {string} url - The url of the site.
+ * @param {string} title - The title of the site.
+ * @param {string} description - The description of the site.
+ */
 app.post('/add', async (req, res) => {
     const url = req.body.url;
     const title = req.body.title;
@@ -18,13 +27,16 @@ app.post('/add', async (req, res) => {
     res.status(httpResponse.statusCode).json(httpResponse.data)
 });
 
-/* Search for sites */
+/**
+ * Search sites
+ * @constructor
+ * @param {string} q - The string text for search in sites
+ */
 app.get('/search', async (req, res) => {
     const search_text = req.query.q?.toString() || '';
     const controller = makeLoadSearchSiteController()
     const httpResponse = await controller.handle(search_text)
     res.status(httpResponse.statusCode).json(httpResponse.data)
 });
-  
 
 app.listen(env.port, () => console.log(`Server running at .:${env.port}`))
